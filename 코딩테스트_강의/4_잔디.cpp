fc642414b2ab51f83ec0545a7f984e448e77a4e6
choiey <choiey073@naver.com>
2022-10-22 22:04:49 +0900

    .

---
+++ 코딩테스트_강의/4_잔디.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+//BFS
+int n, result=INT_MIN;
+int board[31][31], dist[31][31];
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { -1, 0,1, 0 };
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    freopen("input.txt", "rt", stdin);
+    
+    cin >> n;
+    queue <pair <int, int> > Q;
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
+            if (board[i][j] == 2) {
+                Q.push({ i,j });
+            }
+        }
+    }
+
+    while (!Q.empty()) {
+        int x = Q.front().first;
+        int y = Q.front().second;
+        Q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 0) {
+
+                board[nx][ny] = 1;  // 방문처리
+                dist[nx][ny] = dist[x][y] + 1;  
+                Q.push({ nx,ny });
+                result = max(result, dist[nx][ny]);
+            }
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
