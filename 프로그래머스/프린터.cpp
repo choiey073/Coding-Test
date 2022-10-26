6a5591d06a7a140111ee2e420146f09d92ff9981
choiey <choiey073@naver.com>
2022-10-26 22:47:34 +0900

    .

--- 프로그래머스/프린터.cpp
+++ 프로그래머스/프린터.cpp
@@ -1,37 +1,31 @@
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    queue<int> q;
-    int d[10] = {0,};
-    
-    for(int i=0; i<priorities.size(); i++){
-        q.push(i);
-        d[priorities[i]]++;
+    queue< pair<int,int> > q;
+    priority_queue <int> pq;    // 우선순위 큐
+
+    for(int i =0;i<priorities.size();i++){
+        q.push({priorities[i],i});    // i번째 원소의 우선순위와 i를 q에 넣음.
+        pq.push(priorities[i]); // i번째 원소의 우선순위만 넣어주기
     }
     
-    bool flag = true;
-    while(!q.empty()){
-        flag = true;
-        int n = q.front();
-        q.pop();
-        
-        for(int i=priorities[n]+1; i<10; i++){
-            if(d[i]!=0){
-                flag = false;
-                break;
+    while(!q.empty() && !pq.empty()){
+        if(q.front().first == pq.top()){    // q의 우선순위와 pq의 우선순위가 같다면
+            answer++;
+            if(q.front().second == location){   // q의 현재 front와 location이 같으면 return
+                return answer;
             }
-        }
-        
-        if(!flag){
-            q.push(n);
+            q.pop();
+            pq.pop();
         }
         else{
-            answer++;
-            d[priorities[n]]--;
-            if(n == location) return answer;
+            // 해당 원소 맨 뒤로 보내기
+            q.push({q.front().first, q.front().second});
+            q.pop();
         }
     }
 }
