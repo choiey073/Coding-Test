b5f83c2cdb4abbef2d006a874857e71f5cd631f4
choiey <choiey073@naver.com>
2022-11-02 22:25:15 +0900

    .

--- 코딩테스트_강의/6_정사각형그리기.cpp
+++ 코딩테스트_강의/6_정사각형그리기.cpp
@@ -1,34 +1,28 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
 
-    ios_base::sync_with_stdio(false);
+    // ios_base::sync_with_stdio(false);
     freopen("input.txt", "rt", stdin);
 
     int n, m;
     cin >> n >> m;
 
-    vector < string > board;
+    vector < vector <int> > board(n, vector <int>(m, 0));
     vector < vector <int> > dy(n, vector <int>(m, 0));
     vector <int> answer(min(n, m) + 5, 0); // 0번부터 사용, 0 만나면 break;
 
-    for(int i = 0;i<n;i++){
-        string tmp;
-        cin >> tmp;
-        board.push_back(tmp);
-    }
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (board[i][j] == '0')  continue;    // 0이면 그냥 넘기기
+            scanf("%1d", &board[i][j]);
+            if (board[i][j] == 0)  continue;    // 0이면 그냥 넘기기
 
             if (i >= 1 && j >= 1) {   // 경계선 밖으로 나가지 않고, board[i][j]==1이라면
                 dy[i][j] = min(dy[i - 1][j - 1], min(dy[i - 1][j], dy[i][j - 1])) + 1;
             }
             else {  // i가 0이거나 j가 0일때는 i-1, j-1이 경계에서 벗어나므로 그대로 입력
-                dy[i][j] = board[i][j]-'0'; // char을 int로 바꿔줌.
+                dy[i][j] = board[i][j];
             }
         }
     }
@@ -45,7 +39,6 @@ int main() {
         if (answer[i] == 0)    break;
         cout << i << " " << answer[i] << endl;
     }
-    
-    
+
     return 0;
 }
