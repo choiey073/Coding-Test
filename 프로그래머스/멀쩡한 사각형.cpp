2e0aa2679217d3895367f65fd615de2f519ffdf2
choiey <choiey073@naver.com>
2022-11-28 21:34:38 +0900

    .

---
+++ 프로그래머스/멀쩡한 사각형.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+
+// 최대공약수 구하기
+int GCD(int w, int h){
+    int c;
+    while(h != 0){
+        c = w % h;
+        w = h;
+        h = c;
+    }
+    return w;
+}
+
+long long solution(int w,int h) {
+    long long answer = 1;
+    // (w/최대공약수 + h/최대공약수 -1) * 최대공약수
+    // == (w + h - 최대공약수) -> 사용할 수 없는 사각형의 개수
+    
+    // 사용가능한 사각형 == w x h - (w + h - 최대공약수)
+    answer = (long long)w * (long long)h - (w+h);
+    answer += GCD(w,h);
+    
+    return answer;
+}
