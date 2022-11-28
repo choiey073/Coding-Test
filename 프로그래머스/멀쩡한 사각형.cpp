48ce6a417882eacb560c020ed2fd2df701ba5ad0
choiey <choiey073@naver.com>
2022-11-28 21:35:00 +0900

    .

--- 프로그래머스/멀쩡한 사각형.cpp
+++ 프로그래머스/멀쩡한 사각형.cpp
@@ -14,9 +14,8 @@ int GCD(int w, int h){
 long long solution(int w,int h) {
     long long answer = 1;
     // (w/최대공약수 + h/최대공약수 -1) * 최대공약수
-    // == (w + h - 최대공약수) -> 사용할 수 없는 사각형의 개수
-    
-    // 사용가능한 사각형 == w x h - (w + h - 최대공약수)
+    // 사용 불가한 사각형 == (w + h - 최대공약수) -> 사용할 수 없는 사각형의 개수
+    // 사용 가능한 사각형 == w x h - (w + h - 최대공약수)
     answer = (long long)w * (long long)h - (w+h);
     answer += GCD(w,h);
     
