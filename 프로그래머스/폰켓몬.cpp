98979cb989ede50d9c110aa35b6367502e146cf6
choiey <choiey073@naver.com>
2022-12-01 23:32:43 +0900

    .

--- 프로그래머스/폰켓몬.cpp
+++ 프로그래머스/폰켓몬.cpp
@@ -1,20 +1,23 @@
 #include <vector>
 #include <set>
+#include <iostream>
 using namespace std;
 
 int solution(vector<int> nums)
 {
     int answer = 0;
-    int n = nums.size()/2;
+    int N = nums.size()/2; // N마리의 포켓몬 선택.
     set <int> s;
-    for(int i=0; i<nums.size(); i++){
+    for(int i = 0; i < nums.size(); i++){
         s.insert(nums[i]);
     }
-    if(s.size() <= n){
+    
+    if(s.size() <= N){  // 선택해야하는 포켓몬보다 포켓몬 종류가 적은 경우
         answer = s.size();
+        cout << answer;
     }
-    else{
-        answer = n;
+    else{   //  선택해야 하는 포켓몬이 포켓몬 종류보다 많은 경우
+        answer = N;
     }
     
     return answer;
